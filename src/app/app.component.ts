e7291301e42e8dcdb8175044f1770e4b5c8702a5
Ousmane SALL <osall@univ-thies.sn>
2021-11-25 11:47:52 +0000

    Caculatrice MaDSI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projetCalculatrice';
+  tabBoutons = [
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["+/-", "0", ",", "="],
+    ["/", "C", "", ""]
+  ]
+  operande = ""
+  operateur = ""
+  resultat = ""
+  resulatOperation = 0
+
+  calculer(value: any) {
+    if (!isNaN(value)) {
+      this.operande += value
+      this.resultat = this.operande
+    } else {
+      if (value === '+' || value === '-' || value === '*' || value === '/') {
+        this.gestionOperateurCalcul(value)
+      }
+      if (value === "=") {
+        if (this.operateur === "+") {
+          this.resulatOperation += Number.parseInt(this.operande)
+        }
+        if (this.operateur === "*") {
+          this.resulatOperation *= Number.parseInt(this.operande)
+        }
+        if (this.operateur === "-") {
+          this.resulatOperation -= Number.parseInt(this.operande)
+        }
+        if (this.operateur === "/") {
+          this.resulatOperation /= Number.parseInt(this.operande)
+        }
+        this.resultat = this.resulatOperation + ""
+      }
+      if (value === "C") {
+        this.operateur = ""
+        this.resulatOperation = 0
+        this.operande = ""
+        this.resultat = ""
+      }
+    }
+  }
+
+  gestionOperateurCalcul(op: any) {
+    this.operateur = op
+    this.resulatOperation = Number.parseInt(this.operande)
+    this.operande = ""
+  }
 }
